056e0797cde343421e51b037fae33dca6071aa79
Ayush Bharadwaj <4387161+ayusbhar2@users.noreply.github.com>
2022-05-05 17:17:40 -0700

    hessian

--- utils/calculus.py
+++ utils/calculus.py
@@ -6,22 +6,40 @@ def differentiate(f, a, k=1, h=.0001):
 	Args:
 	    f: function whose derivative is to be computed
 	    a: the point at which the derivative is to be computed
-	    k: order of the derivative to be computed
+	    k: order of the derivative to be computed (upto 2)
 	    h: the inifinitesimal to be used in computing the difference quotient
 	Returns:
-	    f^(k)(a)
+	    f^(k)(a): vector if k=1, matrix if k=2.
+
+	NOTE: Assumes that f is twice differentiable and therefore the Hessian is
+		  symmetric.
 	"""
-	# assumes derivative exists
 	n = len(a)
 	grad = np.zeros(n)
-	for i in range(n):
-		v = np.zeros(n)
-		v[i] = h
-		a_r = a + v
-		a_l = a - v
-		q = (f(a_r) - f(a_l))/(2*h)
-		grad[i] = q
-	return grad
+	H = np.zeros([n, n])
+	I = np.identity(n, dtype=float)
+
+	if k == 1:	# re
+		for i in range(n):
+			v = np.zeros(n)
+			v[i] = h
+			a_r = a + v
+			a_l = a - v
+			q = (f(a_r) - f(a_l))/(2*h)
+			grad[i] = q
+		return grad
+
+	elif k == 2:
+		for i in range(n):
+			for j in range(n):
+				sum = 0.0
+				for s in range(2):
+					for t in range(2):
+						sum += (-1)**(s+t) * f(a + h * ((-1)**s * I[i, :] + (-1)**t * I[j, :]))
+				H[i, j] = 1/(2*h)**2 * sum	# TODO: reduce ops, Hessian is symmetric.
+		return H
+	else:
+		raise(ValueError("Order k > 2 not supported."))
 
 
 
