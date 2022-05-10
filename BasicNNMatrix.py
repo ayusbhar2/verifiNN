ed487909c442eeeda4a3c4b2a0101147e15986b5
harshgit <harshbolakani@gmail.com>
2022-05-09 22:16:03 -0400

    removed duplicate methods

--- BasicNNMatrix.py
+++ BasicNNMatrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-num_layers = 4  # total layers
+num_layers = 2  # total layers
 num_features = 2  # total features
 
 
@@ -9,10 +9,10 @@ def main():
     print(f"total layers: {num_layers}")
     print(f"input features : {num_features}")
 
-    input_data = get_training_data(100, num_features)
-    W = init_weights_network(num_layers, num_features)
+    input_data = get_training_data(1, num_features)
+    W_list = init_weights_network(num_layers, num_features)
 
-    output = compute_nn_output_one_epic(input_data, W)
+    output = compute_output(input_data, W_list)
     print(f"output after 1 epic: {output}")
 
 
@@ -36,16 +36,12 @@ def initialize_weights(num_neuron, num_features):
     return np.random.uniform(0.0, 1.0, size=(num_neuron, num_features))
 
 
-def compute_nn_output_one_epic(input_data, weight_list):
-    '''computes and returns y for single data point'''
-    print(f"starting nn for 1 iteration with input {input_data}")
-    print(f"weights:{weight_list}")
-    y = compute_output(input_data, weight_list)
-    return y
-
-
 def compute_output(input_data, weight_list):
     '''computes wn.w(n-1)..w1.(input_data)'''
+    '''computes and returns y for single data point'''
+
+    print(f"starting nn for 1 iteration with input {input_data}")
+    print(f"weights:{weight_list}")
 
     W = compute_weight_multiplication(weight_list=weight_list)
     output = W.dot(input_data.T)
@@ -56,19 +52,19 @@ def compute_output(input_data, weight_list):
 def compute_weight_multiplication(weight_list):
     '''computes wn.w(n-1)..w1, where w(i) is the weight matrix of ith layer'''
 
-    multiplied_weights = np.empty([2, 2])
+    W = np.empty([2, 2])
 
     size = len(weight_list)
     for i in range(size-1, 0, -1):
         if(i == size-1):
-            multiplied_weights = weight_list[i].dot(weight_list[i-1])
+            W = weight_list[i].dot(weight_list[i-1])
         else:
             print(
-                f"computing dot of {multiplied_weights} with {weight_list[i-1]}")
-            multiplied_weights = multiplied_weights.dot(weight_list[i-1])
+                f"computing dot of {W} with {weight_list[i-1]}")
+            W = W.dot(weight_list[i-1])
 
-    print(f"final weights: {multiplied_weights}")
-    return multiplied_weights
+    print(f"final weights: {W}")
+    return W
 
 
 def compute_loss(y, z):
