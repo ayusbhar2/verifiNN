89291aba8f9d0cb36e9e015f2b76d60498afe299
harshgit <harshbolakani@gmail.com>
2022-05-13 18:11:26 -0400

    typo correction

--- utils/network.py
+++ utils/network.py
@@ -2,38 +2,38 @@ import numpy as np
 
 # TODO: maybe we can create an `Network` class later.
 
+
 def generate_network_specs(dx, dz, di, H):
-	"""Generate a list of tuples contianing matrix dimensions.
-	
-	Args:
-		dx (Int): length of the input vector
-		dy (Int): length of output vector
-		di (Int): number of neurons in each hiddeen layer
-		H (Int): number of hidden layers (excluding the output layer)
-	Returns:
-		network_specs (list of tuples): each entry in network_specs is
-			a tuple of the form (num_rows, num_cols). The ith tuple corresponds
-			to the ith weight matrix.
-	"""
-	network_specs = []
-	i = 0
-	while i < H +1:
-		if i == 0:
-			t = (di, dx)
-		elif i == H:
-			t = (dz, di)
-		else:
-			t = (di, di)
-		network_specs.append(t)
-		i += 1
-	return network_specs
+    """Generate a list of tuples contianing matrix dimensions.
+
+    Args:
+            dx (Int): length of the input vector
+            dz (Int): length of output vector
+            di (Int): number of neurons in each hiddeen layer
+            H (Int): number of hidden layers (excluding the output layer)
+    Returns:
+            network_specs (list of tuples): each entry in network_specs is
+                    a tuple of the form (num_rows, num_cols). The ith tuple corresponds
+                    to the ith weight matrix.
+    """
+    network_specs = []
+    i = 0
+    while i < H + 1:
+        if i == 0:
+            t = (di, dx)
+        elif i == H:
+            t = (dz, di)
+        else:
+            t = (di, di)
+        network_specs.append(t)
+        i += 1
+    return network_specs
 
 
 def initialize_network(network_specs):
-	"""Generate random weight matrices from network_specs."""
-	network = []
-	for dims in network_specs:
-		W = np.random.rand(dims[0], dims[1])
-		network.append(W)
-	return network
-
+    """Generate random weight matrices from network_specs."""
+    network = []
+    for dims in network_specs:
+        W = np.random.rand(dims[0], dims[1])
+        network.append(W)
+    return network
