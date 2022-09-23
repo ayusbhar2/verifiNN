bfdef1e81ab12201ed0644e308da238ccca39f31
harshgit <harshbolakani@gmail.com>
2022-09-23 17:52:28 -0400

    added commonutils module and experimental tests

---
+++ utils/commonutils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def unpack_weights(W_List):
+    '''Returns a 1D array by unpacking all weights in the weight list'''
+    flat_list = [w.ravel() for w in W_List]
+    unpacked_array = np.concatenate(flat_list, axis=0)
+    return unpacked_array
+
+
+def pack_weights(w_vector, list_shape_tuple):
+    '''Creates a list of weight matrices form a weights vector accordig to arch.'''
+
+    weight_list = []
+    i = 0
+
+    for shape in list_shape_tuple:
+        size = shape[0]*shape[1]
+        w = w_vector[i:size+i]
+        w_reshaped = w.reshape(shape[0], shape[1])
+        weight_list.append(w_reshaped)
+        i = size
+
+    return weight_list
